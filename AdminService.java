2f1e13811fc6598dee729580bb2cc563750c6777
DESKTOP-PP5G3B7\l <1479079174@qq.com>
2021-01-04 17:25:34 +0800

    2222

--- AdminService.java
+++ AdminService.java
@@ -7,6 +7,7 @@ public interface AdminService {
 
     public Admin login(String adminName,String adminPassword);
 
+  //222
     public Admin get(String adminName,Integer adminId);
 
     public Admin set(String adminName,Integer adminId);
